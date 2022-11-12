3a8b2f006bc65ebf111b8494ca376bc933d5af76
geoffreychen777 <geoffreychen777@gmail.com>
2022-11-12 23:16:52 +0000

    new webpage

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 cd ./cn && npm i && npm run docs:build && cd ..
 cd ./en && npm i && npm run docs:build && cd ..
 mkdir site && mkdir site/cn && mkdir site/en
-cp -r ./cn/docs/.vitepress/dist/* ./site/en/
-cp -r ./en/docs/.vitepress/dist/* ./site/cn/
+cp -r ./cn/docs/.vitepress/dist/* ./site/cn/
+cp -r ./en/docs/.vitepress/dist/* ./site/en/
